e8a0947531497c8df5a0edee1497b7d91d051e1f
Sam O'Leary <ssjoleary@gmail.com>
2014-04-23 21:33:48 +0100

    Post JSON objects

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,10 +1,21 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.InputStream;
+
 
 public class MainActivity {
 
@@ -17,6 +28,8 @@ public class MainActivity {
     private static class RssSightingAsyncTask  {
         RSSParser rssParser = new RSSParser();
         List<RSSItem> rssItems = new ArrayList<RSSItem>();
+        List<Sighting> mySightings = new ArrayList<Sighting>();
+
 
         protected String doInBackground(String... urls) {
             String url = urls[0];
@@ -52,7 +65,8 @@ public class MainActivity {
                             Element latText = doc.select("html body#bd.fs3 div#ja-wrapper div#ja-containerwrap-fr div#ja-containerwrap2 div#ja-container div#ja-container2.clearfix div#ja-mainbody-fr.clearfix div#ja-contentwrap div#ja-content div#k2Container.itemView div.itemBody div.itemFullText div table.table1 tbody tr td").get(16);
                             Element lngText = doc.select("html body#bd.fs3 div#ja-wrapper div#ja-containerwrap-fr div#ja-containerwrap2 div#ja-container div#ja-container2.clearfix div#ja-mainbody-fr.clearfix div#ja-contentwrap div#ja-content div#k2Container.itemView div.itemBody div.itemFullText div table.table1 tbody tr td").get(17);
 
-                            //Sighting sighting = new Sighting(Integer.parseInt(id.ownText()), speciesText.ownText(), date.ownText(), latText.ownText(), lngText.ownText(), location.ownText(), animals.ownText(), nametwo.ownText());
+                            Sighting sighting = new Sighting(Integer.parseInt(id.ownText()), speciesText.ownText(), date.ownText(), latText.ownText(), lngText.ownText(), location.ownText(), animals.ownText(), nametwo.ownText());
+                            mySightings.add(sighting);
                             //wildlifeDB.insertInfoRssSighting(sighting);
                             System.out.println("Debug: " + "Sighting added: species = null");
                             System.out.println("Species: " + speciesText.ownText());
@@ -60,7 +74,8 @@ public class MainActivity {
                             System.out.println("Location: " + location.ownText());
                             System.out.println("----------------");
                         } else {
-                            //Sighting sighting = new Sighting(Integer.parseInt(id.ownText()), species.ownText(), date.ownText(), lat.ownText(), lng.ownText(), location.ownText(), animals.ownText(), name.ownText());
+                            Sighting sighting = new Sighting(Integer.parseInt(id.ownText()), species.ownText(), date.ownText(), lat.ownText(), lng.ownText(), location.ownText(), animals.ownText(), name.ownText());
+                            mySightings.add(sighting);
                             //wildlifeDB.insertInfoRssSighting(sighting);
                             System.out.println("Debug: " + "Sighting added: species = null");
                             System.out.println("Species: " + species.ownText());
@@ -70,6 +85,9 @@ public class MainActivity {
                             System.out.println("Debug: " + "Sighting added: species != null");
 
                         }
+                            for(Sighting subSighting: mySightings) {
+                                showSuccess(POST("http://fyp-irish-wildlife.herokuapp.com/sightings/postsighting/", subSighting));
+                            }
                     } else {
                         System.out.println("Debug: " + "Sighting not added");
                         return null;
@@ -83,6 +101,107 @@ public class MainActivity {
         private boolean checkSightingExists(String s) {
 
             return false;
+        }
+        public static String POST(String url, Sighting sightingSubmit){
+            InputStream inputStream;
+            String result = "";
+            try {
+
+                // 1. create HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+
+                // 2. make POST request to the given URL
+                HttpPost httpPost = new HttpPost(url);
+
+                String json;
+
+                // 3. build jsonObject
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("species", sightingSubmit.getSpecies());
+                jsonObject.put("date", sightingSubmit.getDate());
+                jsonObject.put("lat", sightingSubmit.getSightingLat());
+                jsonObject.put("lng", sightingSubmit.getSightingLong());
+                jsonObject.put("location", sightingSubmit.getLocation());
+                jsonObject.put("animals", sightingSubmit.getAnimals());
+                jsonObject.put("name", sightingSubmit.getName());
+                jsonObject.put("imageurl", sightingSubmit.getImgUrlString());
+
+                // 4. convert JSONObject to JSON to String
+                json = jsonObject.toString();
+                // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+                // ObjectMapper mapper = new ObjectMapper();
+                // json = mapper.writeValueAsString(searchActivity);
+
+                // 5. set json to StringEntity
+                StringEntity se = new StringEntity(json);
+
+                // 6. set httpPost Entity
+                httpPost.setEntity(se);
+
+                // 7. Set some headers to inform server about the type of the content
+                httpPost.setHeader("Accept", "application/json");
+                httpPost.setHeader("Content-type", "application/json");
+
+                // 8. Execute POST request to the given URL
+                HttpResponse httpResponse = httpclient.execute(httpPost);
+
+                // 9. receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // 10. convert inputstream to string
+                if(inputStream != null) {
+                    System.out.println("Debug: SubmitActivity: POST: inputStream != null");
+                    result = convertInputStreamToString(inputStream);
+                }
+                else {
+                    System.out.println("Debug: SubmitActivity: POST: inputStream = null: did not work");
+                    result = "Did not work!";
+                }
+
+            } catch (Exception e) {
+                System.out.println("Debug: InputStream" + e.getLocalizedMessage());
+            }
+
+            // 11. return result
+            return result;
+        }
+
+        private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+            String line;
+            StringBuilder result = new StringBuilder();
+            while((line = bufferedReader.readLine()) != null) {
+                result.append(line);
+            }
+
+            inputStream.close();
+            System.out.println("Debug: SubmitActivity: convertInputStreamToString: result.To.String(): " + result.toString());
+
+            return result.toString();
+
+        }
+
+        private void showSuccess(String result) {
+            //generateNoteOnSD("ErrorHtml", result);
+            Object obj = JSONValue.parse(result);
+            JSONArray json;
+            try {
+                json = (JSONArray)obj;
+                for (int i = 0; i < json.size(); i++) {
+                    JSONObject c;
+                    try {
+                        c = (JSONObject)json.get(i);
+
+                        //String success = c.get("success");
+                        System.out.println("Debug: isSuccessful: " + c.get(i));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
---
+++ src/Sighting.java
@@ -0,0 +1,134 @@
+import java.io.Serializable;
+
+/**
+ * Author: Sam O'Leary
+ * Email: somhairle.olaoire@mycit.ie
+ * Created: 12/02/14
+ * Revision: 1
+ * Revision History:
+ * 1: 12/02/14
+ * Description:
+ */
+public class Sighting implements Serializable {
+    private final static long serialVersionUID = 1;
+
+    private int ID;
+    private String name;
+    private String species;
+    private String date;
+    private double sightingLat;
+    private double sightingLong;
+    private String location;
+    private int animals;
+    private String imgUrl = "image";
+
+    public Sighting(int ID, String species, String date, double sightingLat, double sightingLong, String location, int animals, String name, String imgUrl) {
+        this.ID = ID;
+        this.species = species;
+        this.date = date;
+        this.sightingLat = sightingLat;
+        this.sightingLong = sightingLong;
+        this.location = location;
+        this.animals = animals;
+        this.name = name;
+        this.imgUrl = imgUrl;
+    }
+
+    public Sighting(int ID, String species, String date, String sightingLat, String sightingLong, String location, String animals, String name) {
+        this.ID = ID;
+        if(species.equals("")){
+            this.species = "N/A";
+        } else {
+            this.species = species;
+        }
+        if(date.equals("")){
+            this.date = "N/A";
+        } else {
+            this.date = date;
+        }
+        if(sightingLat.equals("")){
+            this.sightingLat = 0;
+        } else {
+            try{
+                this.sightingLat = Double.parseDouble(sightingLat);
+            } catch (NumberFormatException e) {
+                this.sightingLong = 0;
+                System.out.println("Debug: NumberFormatException: Sighting.java: 62");
+            }
+        }
+        if(sightingLong.equals("")){
+            this.sightingLong = 0;
+        } else {
+            try {
+                this.sightingLong = Double.parseDouble(sightingLong);
+            } catch (NumberFormatException e) {
+                this.sightingLong = 0;
+                System.out.println("Debug: NumberFormatException: Sighting.java: 72");
+            }
+        }
+        if(location.equals("")){
+            this.location = "N/A";
+        } else {
+            this.location = location;
+        }
+        if(animals.equals("")){
+            this.animals = 0;
+        } else {
+            this.animals = Integer.parseInt(animals);
+        }
+        if(name.equals("")){
+            this.name = "N/A";
+        } else {
+            this.name = name;
+        }
+    }
+
+    /*public Sighting(String species, String date, double sightingLat, double sightingLong, String location, int animals) {
+        this.species = species;
+        this.date = date;
+        this.sightingLat = sightingLat;
+        this.sightingLong = sightingLong;
+        this.location = location;
+        this.animals = animals;
+    }
+    */
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getSightingLat() {
+        return sightingLat;
+    }
+
+    public double getSightingLong() {
+        return sightingLong;
+    }
+
+    public int getAnimals() {
+        return animals;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getImgUrlString() {
+        if (imgUrl == null) {
+            imgUrl = "image";
+            return imgUrl;
+        }
+        return imgUrl;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
